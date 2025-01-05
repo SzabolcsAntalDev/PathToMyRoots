cf409420482cecb127fc0179e840eeee1b543dc9
Szabolcs Antal <szabolcs.antal.dev@gmail.com>
2025-01-06 00:01:45 +0200

    Configure the connection string using the web app builder

--- PathToMyRootsApi/Program.cs
+++ PathToMyRootsApi/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using PathToMyRootsDataAccess.Models;
+
 namespace PathToMyRootsApi
 {
     public class Program
@@ -6,8 +9,8 @@ namespace PathToMyRootsApi
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-
+            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+            builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
             builder.Services.AddControllers();
 
             var app = builder.Build();
