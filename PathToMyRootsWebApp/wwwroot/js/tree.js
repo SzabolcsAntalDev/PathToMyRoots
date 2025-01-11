fad39e8349c39d78c9721f2ed3dd2b72e6d306a4
Szabolcs Antal <szabolcs.antal.dev@gmail.com>
2025-01-12 01:22:07 +0200

    Add scrollbar to tree and edgy lines

--- PathToMyRootsWebApp/wwwroot/js/tree.js
+++ PathToMyRootsWebApp/wwwroot/js/tree.js
@@ -1,5 +1,6 @@
 ﻿const nodeWidth = 100;
 const nodeHeight = 50;
+const linesVerticalOffset = 4;
 
 const apiUrl = "https://localhost:7241/api/person/getfamily/";
 
@@ -19,23 +20,29 @@ async function createTreeDiagram(personId) {
     //for (let level of sortedKeys)
     //    treeDiagram1.appendChild(divsOnLevels[level]);
 
-
-
-
     const treeDiagram2 = document.getElementById('tree-diagram2');
-    let rootParentsLevel = sortedKeys[0]
+    const rootParentsLevel = sortedKeys[0]
+    let parentsLevelDivsCollection = [];
     let parentsLevelDiv = divsOnLevels[rootParentsLevel];
+    parentsLevelDivsCollection.push(parentsLevelDiv);
     treeDiagram2.appendChild(parentsLevelDiv);
 
     // sort children
     for (let i = 1; i < sortedKeys.length; i++) {
         const childrenLevel = sortedKeys[i];
-        let childrenLevelDiv = divsOnLevels[childrenLevel];
+        const childrenLevelDiv = divsOnLevels[childrenLevel];
 
         parentsLevelDiv = sortChildrenLevelDiv(treeDiagram2, parentsLevelDiv, childrenLevelDiv);
+        parentsLevelDivsCollection.push(parentsLevelDiv);
     }
 
     // draw lines
+    for (let i = 1; i < parentsLevelDivsCollection.length; i++) {
+        const parentsLevelDiv1 = parentsLevelDivsCollection[i - 1];
+        const childrenLevelDiv = parentsLevelDivsCollection[i];
+
+        drawLines(parentsLevelDiv1, childrenLevelDiv);
+    }
 }
 
 function sortChildrenLevelDiv(treeDiagram2, parentsLevelDiv, childrenLevelDiv) {
@@ -47,21 +54,92 @@ function sortChildrenLevelDiv(treeDiagram2, parentsLevelDiv, childrenLevelDiv) {
         let fatherId = parentsDiv.querySelector('.tree-node-male')?.id;
         let motherId = parentsDiv.querySelector('.tree-node-female')?.id;
 
-        let childDivs = childrenLevelDiv.querySelectorAll('.tree-div');
-        childDivs.forEach(childDiv => {
-            let malesChildBiologicalMotherIdId = childDiv.querySelector('.tree-node-male')?.biologicalMotherId;
-            let malesChildBiologicalFatherId = childDiv.querySelector('.tree-node-male')?.biologicalFatherId;
-            let femalesChildBiologicalMotherIdId = childDiv.querySelector('.tree-node-female')?.biologicalMotherId;
-            let femalesChildBiologicalFatherId = childDiv.querySelector('.tree-node-female')?.biologicalFatherId;
+        let pairDivs = childrenLevelDiv.querySelectorAll('.tree-div');
+        pairDivs.forEach(pairDiv => {
+            let malesChildBiologicalMotherId = pairDiv.querySelector('.tree-node-male')?.biologicalMotherId;
+            let malesChildBiologicalFatherId = pairDiv.querySelector('.tree-node-male')?.biologicalFatherId;
+            let femalesChildBiologicalMotherId = pairDiv.querySelector('.tree-node-female')?.biologicalMotherId;
+            let femalesChildBiologicalFatherId = pairDiv.querySelector('.tree-node-female')?.biologicalFatherId;
 
-            if (motherId == malesChildBiologicalMotherIdId || motherId == femalesChildBiologicalMotherIdId ||
-                fatherId == malesChildBiologicalFatherId || fatherId == femalesChildBiologicalFatherId) {
-                sortedChildrenLevelDiv.appendChild(childDiv);
+            if (fatherId == malesChildBiologicalFatherId || fatherId == femalesChildBiologicalFatherId ||
+                motherId == malesChildBiologicalMotherId || motherId == femalesChildBiologicalMotherId) {
+                sortedChildrenLevelDiv.appendChild(pairDiv);
             }
         });
     });
 
     return sortedChildrenLevelDiv;
+}
+
+function drawLines(parentsLevelDiv, childrenLevelDiv) {
+    svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+    svg.id = 'tree-lines-svg';
+    svg.style.position = 'absolute';
+    svg.style.top = '0';
+    svg.style.left = '0';
+    svg.style.width = '100%';
+    svg.style.height = '100%';
+    svg.style.pointerEvents = 'none';
+    svg.style.border = "2px solid red";
+    document.body.appendChild(svg);
+
+    let parentsDivs = parentsLevelDiv.querySelectorAll('.tree-div');
+    let childrenDivs = childrenLevelDiv.querySelectorAll('.tree-div');
+
+    let i = -5 * linesVerticalOffset;
+
+    parentsDivs.forEach(parentsDiv => {
+        let fatherId = parentsDiv.querySelector('.tree-node-male')?.id;
+        let motherId = parentsDiv.querySelector('.tree-node-female')?.id;
+
+        let pairDivs = childrenLevelDiv.querySelectorAll('.tree-div');
+
+        pairDivs.forEach(pairDiv => {
+            let pairMaleNode = pairDiv.querySelector('.tree-node-male');
+            let pairFemaleNode = pairDiv.querySelector('.tree-node-female');
+
+            let malesBiologicalFatherId = pairMaleNode?.biologicalFatherId;
+            let malesBiologicalMotherId = pairMaleNode?.biologicalMotherId;
+            let femalesBiologicalFatherId = pairFemaleNode?.biologicalFatherId;
+            let femalesBiologicalMotherId = pairFemaleNode?.biologicalMotherId;
+
+            if (pairMaleNode != null && (fatherId == malesBiologicalFatherId || motherId == malesBiologicalMotherId)) {
+                drawLine(parentsDiv, pairMaleNode, svg, i += linesVerticalOffset);
+            }
+
+            if (pairFemaleNode != null && (fatherId == femalesBiologicalFatherId || motherId == femalesBiologicalMotherId)) {
+                drawLine(parentsDiv, pairFemaleNode, svg, i += linesVerticalOffset);
+            }
+        });
+    });
+}
+
+function drawLine(parentElement, childElement, svg, verticalOffset) {
+    const parentRect = parentElement.getBoundingClientRect();
+    const childRect = childElement.getBoundingClientRect();
+
+    const parentX = parentRect.left + parentRect.width / 2;
+    const parentY = parentRect.top + parentRect.height;
+    const childX = childRect.left + childRect.width / 2;
+    const childY = childRect.top;
+
+    const middleY = ((parentY + childY) / 2) + verticalOffset;
+
+    // Draw the path
+    const pathData = `
+        M ${parentX},${parentY}
+        L ${parentX},${middleY}
+        L ${childX},${middleY}
+        L ${childX},${childY}
+    `;
+
+    const path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+    path.setAttribute('d', pathData);
+    path.setAttribute('stroke', 'black');
+    path.setAttribute('stroke-width', '1');
+    path.setAttribute('fill', 'transparent'); // No fill for the path
+
+    svg.appendChild(path);
 }
 
 async function createNodesFrom(personId, processedPersonIds, divsOnLevels, level) {
@@ -116,7 +194,7 @@ function createTreeNode(person) {
     treeNode.biologicalMotherId = person.biologicalMotherId;
     treeNode.biologicalFatherId = person.biologicalFatherId;
     treeNode.className = person.isMale ? 'tree-node-male' : 'tree-node-female';
-    treeNode.innerText = `${person.lastName} ${person.firstName}`;
+    treeNode.innerText = `${person.id} - ${person.lastName} ${person.firstName}`;
     return treeNode;
 }
 
