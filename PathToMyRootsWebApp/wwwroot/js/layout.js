bb9f7d567a6ed934adf596164281539e95d37e25
Szabolcs Antal <szabolcs.antal.dev@gmail.com>
2025-01-26 02:15:02 +0200

    Add light and dark themes

---
+++ PathToMyRootsWebApp/wwwroot/js/layout.js
@@ -0,0 +1,16 @@
+﻿const lightThemeButton = document.getElementById('light-theme-button');
+const darkThemeButton = document.getElementById('dark-theme-button');
+
+function applyTheme(theme) {
+    document.documentElement.classList.remove('theme-light', 'theme-dark');
+    document.documentElement.classList.add(theme);
+    localStorage.setItem('theme', theme);
+}
+
+lightThemeButton.addEventListener('click', () => {
+    applyTheme('theme-light');
+});
+
+darkThemeButton.addEventListener('click', () => {
+    applyTheme('theme-dark');
+});
