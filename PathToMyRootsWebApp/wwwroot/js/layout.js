6dc0ef323c18d06518666d485a3d9f10456e65c5
Szabolcs Antal <szabolcs.antal.dev@gmail.com>
2025-01-28 23:37:50 +0200

    Use correct theme on page load

--- PathToMyRootsWebApp/wwwroot/js/layout.js
+++ PathToMyRootsWebApp/wwwroot/js/layout.js
@@ -4,6 +4,10 @@ const themeSpan = document.getElementById('theme-span');
 function applyTheme(theme) {
     document.documentElement.classList.remove('theme-light', 'theme-dark');
     document.documentElement.classList.add(theme);
+
+    themeToggle.checked = theme === 'theme-dark';
+    themeSpan.textContent = theme === 'theme-dark' ? "Dark Theme" : "Light Theme";
+
     localStorage.setItem('theme', theme);
 }
 
@@ -17,3 +21,10 @@ themeToggle.addEventListener('change', () => {
         applyTheme('theme-light');
     }
 });
+
+function initializeTheme() {
+    const savedTheme = localStorage.getItem('theme') || 'theme-light';
+    applyTheme(savedTheme);
+}
+
+initializeTheme();
