d967f82c908de50c1a98e0f46f5487161d660b5b
Szabolcs Antal <szabolcs.antal.dev@gmail.com>
2025-02-09 22:37:07 +0200

    Let interchange spouses

--- PathToMyRootsWebApp/wwwroot/js/add-edit-person/select.js
+++ PathToMyRootsWebApp/wwwroot/js/add-edit-person/select.js
@@ -13,14 +13,37 @@ function secondSpouseIdChanged() {
 
 function updateSpouseSelects(idleSelectName, changedSelectName) {
     const isMale = document.getElementById('is-male-select').value.toLowerCase();
+    const idleSelect = document.getElementById(idleSelectName + '-spouse-select');
     const changedSelect = document.getElementById(changedSelectName + '-spouse-select');
+
     const changedSelectValue = changedSelect.value;
 
     document.querySelectorAll('#' + idleSelectName + '-spouse-select option').forEach(option => {
-        option.style.display =
-            (option.value !== "" && (option.dataset.isMale.toLowerCase() === isMale || option.value == changedSelectValue))
-                ? 'none'
-                : '';
+
+        if (option.value == "") {
+            option.style.display = '';
+            return;
+        }
+
+        if (option.dataset.isMale.toLowerCase() === isMale) {
+            option.style.display = 'none';
+            return;
+        }
+
+        if (option.value == changedSelectValue) {
+            option.style.display = 'none';
+            return;
+        }
+
+        if (option.dataset.firstSpouseId &&
+            option.dataset.secondSpouseId &&
+            option.dataset.firstSpouseId != idleSelect.dataset.personId &&
+            option.dataset.secondSpouseId != idleSelect.dataset.personId) {
+            option.style.display = 'none';
+            return;
+        }
+
+        option.style.display = '';
     });
 
     const changedSpouseMarriageDatesContainer = document.getElementById(changedSelectName + '-spouse-marriage-dates-container');
