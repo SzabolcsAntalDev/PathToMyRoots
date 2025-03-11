23c4d120bc14bd04f00d48a4a570f4672b17a10a
Szabolcs Antal <szabolcs.antal.dev@gmail.com>
2025-03-11 21:02:50 +0200

    Reset hidden input image when crop is canceled

--- PathToMyRootsWebApp/wwwroot/js/add-edit-person/image.js
+++ PathToMyRootsWebApp/wwwroot/js/add-edit-person/image.js
@@ -1,26 +1,26 @@
-﻿const apiUrl = "https://localhost:7241/";
+﻿const apiUrl = 'https://localhost:7241/';
 
-const hiddenImageInputUrl = document.getElementById("hidden-image-input-url");
-const hiddenImageInput = document.getElementById("hidden-image-input");
-const selectImageButton = document.getElementById("select-image-button");
+const hiddenImageInputUrl = document.getElementById('hidden-image-input-url');
+const hiddenImageInput = document.getElementById('hidden-image-input');
+const selectImageButton = document.getElementById('select-image-button');
 
-const toggleablePreviewImageContainer = document.getElementById("toggleable-preview-image-container");
-const previewImage = document.getElementById("preview-image");
-const removeImageButton = document.getElementById("remove-image-button");
+const toggleablePreviewImageContainer = document.getElementById('toggleable-preview-image-container');
+const previewImage = document.getElementById('preview-image');
+const removeImageButton = document.getElementById('remove-image-button');
 
-const cropperBackgroundContainer = document.getElementById("cropper-background-container");
-const imageToCrop = document.getElementById("image-to-crop");
-const confirmCropButton = document.getElementById("confirm-crop-button");
-const cancelCropButton = document.getElementById("cancel-crop-button");
+const cropperBackgroundContainer = document.getElementById('cropper-background-container');
+const imageToCrop = document.getElementById('image-to-crop');
+const confirmCropButton = document.getElementById('confirm-crop-button');
+const cancelCropButton = document.getElementById('cancel-crop-button');
 
 let cropper;
 
-selectImageButton.addEventListener("click", (event) => {
+selectImageButton.addEventListener('click', (event) => {
     event.preventDefault();
     hiddenImageInput.click();
 });
 
-hiddenImageInput.addEventListener("change", (event) => {
+hiddenImageInput.addEventListener('change', (event) => {
     const imageFile = event.target.files[0];
     const fileReader = new FileReader();
 
@@ -42,18 +42,18 @@ hiddenImageInput.addEventListener("change", (event) => {
     fileReader.readAsDataURL(imageFile);
 });
 
-confirmCropButton.addEventListener("click", async (event) => {
+confirmCropButton.addEventListener('click', async (event) => {
     event.preventDefault();
 
     const croppedCanvas = cropper.getCroppedCanvas();
 
     croppedCanvas.toBlob(async (blob) => {
         const formData = new FormData();
-        formData.append("image", blob, "cropped-image.png");
+        formData.append('image', blob, 'cropped-image.png');
 
         await deleteImage(hiddenImageInputUrl.value);
 
-        const response = await fetch(apiUrl + "api/image/upload/", {
+        const response = await fetch(apiUrl + 'api/image/upload/', {
             method: 'POST',
             body: formData,
         });
@@ -64,12 +64,13 @@ confirmCropButton.addEventListener("click", async (event) => {
         updatePreviewImage();
 
         cropperBackgroundContainer.style.display = 'none';
-        hiddenImageInput.value = "";
-    }, "image/png");
+        hiddenImageInput.value = '';
+    }, 'image/png');
 });
 
-cancelCropButton.addEventListener("click", async (event) => {
+cancelCropButton.addEventListener('click', async (event) => {
     event.preventDefault();
+    hiddenImageInput.value = '';
     cropperBackgroundContainer.style.display = 'none';
 });
 
@@ -82,16 +83,16 @@ function updatePreviewImage() {
     }
 
     toggleablePreviewImageContainer.classList.add('toggleable-container-open');
-    previewImage.src = "https://localhost:7241/api/Image/get/" + imageUrl;
+    previewImage.src = 'https://localhost:7241/api/Image/get/' + imageUrl;
     removeImageButton.removeAttribute('disabled');
 }
 
-removeImageButton.addEventListener("click", async (event) => {
+removeImageButton.addEventListener('click', async (event) => {
     event.preventDefault();
 
     await deleteImage(hiddenImageInputUrl.value);
 
-    hiddenImageInputUrl.value = "";
+    hiddenImageInputUrl.value = '';
     updatePreviewImage();
 });
 
@@ -102,6 +103,6 @@ async function deleteImage(imageName) {
 
     var url = apiUrl + `api/image/delete/${imageName}`;
     await fetch(url, {
-        method: "DELETE",
+        method: 'DELETE',
     });
 }
