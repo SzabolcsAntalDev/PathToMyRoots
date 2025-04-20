ecd2b067bd8e7317bff5605bddaab0e99db52e4b
Szabolcs Antal <szabolcs.antal.dev@gmail.com>
2025-04-20 03:01:48 +0300

    Add two initial tests

---
+++ PathToMyRootsWebApp/Controllers/TestController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PathToMyRootsWebApp.Services;
+
+namespace PathToMyRootsWebApp.Controllers
+{
+    public class TestController : Controller
+    {
+        private readonly PersonApiService _personApiService;
+
+        public TestController(PersonApiService personApiService)
+        {
+            _personApiService = personApiService;
+        }
+
+        public IActionResult Tests()
+        {
+            return View();
+        }
+    }
+}
