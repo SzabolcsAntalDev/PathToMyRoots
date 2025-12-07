24abdad1078a7e309432a7fca2f40f802641951f
Szabolcs Antal <szabolcs.antal.dev@gmail.com>
2025-12-07 20:33:56 +0200

    Add writeCurrentTimeOnConsole

--- PathToMyRootsWebApp/wwwroot/js/common/utils.js
+++ PathToMyRootsWebApp/wwwroot/js/common/utils.js
@@ -101,3 +101,14 @@ function getSize(element) {
 function arrayRemoveDuplicatesWithSameId(array) {
     return Array.from(new Map(array.map(obj => [obj.id, obj])).values());
 }
+
+function writeCurrentTimeOnConsole(additionalText) {
+    const now = new Date();
+
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    const seconds = String(now.getSeconds()).padStart(2, '0');
+    const milliseconds = String(now.getMilliseconds()).padStart(3, '0');
+
+    console.log(`${hours}:${minutes}:${seconds}.${milliseconds}` + additionalText ? ` - ${additionalText}` : '');
+}
