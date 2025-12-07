c3f8e160fba845609a433c6d0c039449293827be
Szabolcs Antal <szabolcs.antal.dev@gmail.com>
2025-12-07 20:33:20 +0200

    Center loading text in visible area of container

--- PathToMyRootsWebApp/wwwroot/js/common/loading-text.js
+++ PathToMyRootsWebApp/wwwroot/js/common/loading-text.js
@@ -16,28 +16,75 @@
             $('<label>')
                 .attr('class', 'custom-loading-text-label');
 
+        const loadingTextDisableMask =
+            $('<div>')
+                .attr('class', 'loading-text-disable-mask');
+
         loadingTextContainer =
             $('<div>')
                 .attr('class', 'loading-text-container')
                 .append(animatedLoadingTextDiv)
                 .append(customLoadingTextLabel);
 
+        loadingTextContainer.loadingTextDisableMask = loadingTextDisableMask;
+
+        parent.append(loadingTextDisableMask);
+        parent.append(loadingTextContainer);
+
+        const grandParent = parent.parent();
+
+        if (window.MutationObserver) {
+            const observer = new MutationObserver(() => this.centerLoadingTextContainer(loadingTextContainer));
+            observer.observe(grandParent[0], { childList: true, subtree: true, characterData: true });
+        }
+
         hideElement(loadingTextContainer);
 
         return loadingTextContainer;
     },
 
-    async fadeIn(loadingTextContainer) {
+    centerLoadingTextContainer(loadingTextContainer) {
+        const parentRect = loadingTextContainer.parent()[0].getBoundingClientRect();
+        const grandParentRect = loadingTextContainer.parent().parent()[0].getBoundingClientRect();
+
+        // Intersection between parent and grandParent
+        const visibleLeft = Math.max(parentRect.left, grandParentRect.left);
+        const visibleRight = Math.min(parentRect.right, grandParentRect.right);
+        const visibleTop = Math.max(parentRect.top, grandParentRect.top);
+        const visibleBottom = Math.min(parentRect.bottom, grandParentRect.bottom);
+
+        // the relatives column has a padding of thickness-large
+        // so it shown a horizontal scrollbar if the loadingTextContainer is larger than the inner content
+        const thicknessLargeValue = getThicknessLargeValue();
+
+        const visibleWidth = visibleRight - visibleLeft - (2 * thicknessLargeValue);
+        const visibleHeight = visibleBottom - visibleTop - (2 * thicknessLargeValue);
+
+        const parentLeft = visibleLeft - parentRect.left + thicknessLargeValue;
+        const parentTop = visibleTop - parentRect.top + thicknessLargeValue;
+
+        loadingTextContainer.css({
+            left: parentLeft + 'px',
+            top: parentTop + 'px',
+            width: visibleWidth + 'px',
+            height: visibleHeight + 'px'
+        });
+    },
+
+    async fadeIn(loadingTextContainer, text) {
         // set the custom text to empty, so when
         // the control below it fades in the old text is not shown
-        this.setLoadingProgressText(loadingTextContainer.parent(), '');
-        this.startAnimating($(loadingTextContainer));
-        await fadeInElement($(loadingTextContainer));
+        this.setLoadingProgressText(loadingTextContainer.parent(), text);
+        fadeInElement(loadingTextContainer.loadingTextDisableMask);
+
+        this.startAnimating(loadingTextContainer);
+        await fadeInElement(loadingTextContainer);
     },
 
     async fadeOut(loadingTextContainer) {
-        await fadeOutElement($(loadingTextContainer));
-        this.stopAnimating($(loadingTextContainer));
+        fadeOutElement(loadingTextContainer.loadingTextDisableMask);
+        await fadeOutElement(loadingTextContainer);
+        this.stopAnimating(loadingTextContainer);
     },
 
     startAnimating(loadingTextContainer) {
